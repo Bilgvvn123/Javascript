50ee9e55e79b9f8eb510b96653c25cdc6fb8c41e
Bilgvvn123 <dukd875@gmail.com>
2023-10-02 15:18:47 +0800

    anhnii js hicheel

---
+++ lesson_1/1_primitive_datatype.js
@@ -0,0 +1,68 @@
+// kub
+// var a = 5;
+
+// console.log(a * a * a, a * a * 6);
+
+// tegsh untsugt
+// var a = 6,
+// 	b = 4;
+// console.log(a * b, (a + b) * 2);
+
+// func #1
+// var x = 1;
+// var y = 3 * x - 5;
+// console.log(y);
+
+// func #2
+// var x = -2;
+// var y = 4 * x * x - 3 * x + 5;
+// console.log(y);
+
+// var a = 476;
+// var b = a % 10;
+// console.log(b);
+
+// sec to min
+// console.log(Math.floor(5.5));
+
+// 10t-iin ornii tsifr
+
+// a = a / 10;
+// a = Math.floor(a);
+
+// console.log(a);
+
+// 2 oronotoi toonii vrjver
+// var a = prompt("Toogoo oruulna uu ");
+
+// console.log(typeof );
+// a = Number(a) + 1;
+// console.log(a)
+
+// var a = Number(prompt("3n orontoi too oruulna uu"));
+
+// var b, c, d;
+// b = Math.floor(a / 100);
+// c = a % 100;
+// c = Math.floor(c / 10);
+// d = a % 10;
+
+// console.log(b + c + d);
+
+// console.log(a + b + c);
+
+// min, sec to sec
+
+// console.log(Number(prompt("min-aa")) * 60 + Number(prompt("sec-aa")));
+
+// 2 toony ihiig olno uu
+// var a = Number(prompt("a toogoo oruulna uu"));
+// var b = Number(prompt("b toogoo oruulna uu"));
+
+// if (a > b) {
+// 	console.log("a ni ih baina: ", a);
+// } else if (a == 99) {
+// 	console.log("fghjkl;'");
+// } else {
+// 	console.log("b ni ih baina: ", b);
+// }
