5625407daca0d1bda1810cd1f6b698b5c9a06750
Bilgvvn123 <dukd875@gmail.com>
2023-10-03 11:15:24 +0800

    second commit

---
+++ lesson_2_coursion/index.js
@@ -0,0 +1,27 @@
+// primitive - engiin ogogdliin torlvvd , reference - zaalt olonlog
+// TODO
+// primitive ogogdliin torlvvd
+// number, string, boolean, undefined, null;
+// var a = ""[
+// 	// reference - olonlog
+// 	// array, object
+// 	(1, 2, "")
+// ];
+
+// var x = {
+// 	boldoo: 1,
+// 	ganaa: "fghjkl;",
+// };
+
+// TODO undefined
+// var a;
+// console.log(a);
+
+// TODO null
+// var a;
+
+// a = 5;
+// a = "  ";
+// a = true;
+
+// a = null;
