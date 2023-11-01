ac2d7c83dca79a4d070fe8e2496a3cb804a2d5a2
Bilgvvn123 <dukd875@gmail.com>
2023-11-01 13:52:15 +0800

    ss

--- lesson23_class/class.js
+++ lesson23_class/class.js
@@ -26,3 +26,52 @@
 
 // console.log(Jessi2.iddeg());
 // console.log();
+
+// class Eej {
+// 	constructor() {}
+
+// 	duuldag() {
+// 		console.log("goi dyyldag");
+// 	}
+
+// 	mashGoiHoolHiideg() {
+// 		console.log("goi hool hiideg");
+// 	}
+// }
+
+// class Bi extends Eej {
+// 	kodeBicdeg() {
+// 		console.log("KOD bicdeg");
+// 	}
+// }
+
+// // const a = new Bi();
+
+// class Text {
+// 	constructor(text) {
+// 		this.text = text;
+// 		this.arr = [];
+// 	}
+
+// 	Split() {
+// 		for (let i = 0; i < this.text.length; i++) {
+// 			this.arr.push(this.text[i]);
+// 		}
+
+// 		return this.arr;
+// 	}
+// }
+
+// // console.log("aaa".split(""));
+// const a = new Text("hello");
+// console.log(a.Split());
+
+const a = [1, 2, 3];
+
+a = 3;
+
+if (a == 3) {
+	let b, c;
+
+	b + c;
+}
