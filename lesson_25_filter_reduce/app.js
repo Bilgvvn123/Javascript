a141a340549f7dfa19783604bf217d9f5a9985be
Bilgvvn123 <dukd875@gmail.com>
2023-11-02 13:46:56 +0800

    aaa

---
+++ lesson_25_filter_reduce/app.js
@@ -0,0 +1,67 @@
+const a = [4, 6, 10, 22, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1];
+// let s = 1;
+// a.forEach((el) => {
+// 	console.log(el);
+// });
+
+// let x = a.filter((el) => {
+// 	el = el + 1;
+// 	return el % 2 == 0;
+// });
+// console.log(x);
+
+// function aa(e) {
+// 	s = s * e;
+// }
+// for (let i = 0; i < a.length; i++) {
+// 	aa(a[i]);
+// }
+
+// console.log(s);
+
+// const a = [1, 2, 3];
+// const b = [2, 4];
+
+// const c = [...a, ...b];
+
+// console.log(c);
+// function aa(...a) {
+// 	console.log(a);
+// }
+
+// aa(1, 2, 3, 5);
+
+// const arr = [1, 2, 3, 4, 5];
+// let arr2 = arr.map((el) => {
+// 	console.log(el);
+// 	return el + 5;
+// });
+// console.log(arr2);
+
+// let arr3 = arr.filter((el) => {
+// 	console.log(el);
+// });
+
+// console.log(arr3);
+
+// let arr4 = arr.forEach((el) => {
+// 	console.log(el);
+// });
+
+// const arr = ["halo", "hello", "hi"];
+// const arr2 = [];
+
+// for (let i = 0; i < arr.length; i++) {
+// 	let el = `<p>${arr[i]}</p>`;
+// 	arr2.push(el);
+// }
+// console.log(arr2);
+
+// let arr3 = arr.map((el) => `<p>${el}</p>`);
+// let arr3 = arr.map(function (el) {
+// 	// console.log(el);
+// 	let a = el + 10000000000;
+// 	return a;
+// });
+
+// console.log(arr3);
